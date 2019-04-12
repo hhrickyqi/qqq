5f9247efc407ad27489ee8a6534c4bcf4ec581e1
hhrickyqi <455735031@qq.com>
2019-04-12 22:00:20 +0800

    a

--- src/Demo10.java
+++ src/Demo10.java
@@ -1,5 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Demo10 {
     public static void main(String[] args) {
-
+        int[] a = {21, 87, -5, 16};
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++)
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+        }
+        System.out.println(Arrays.toString(a));
     }
 }
