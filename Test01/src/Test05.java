8d71efaad07eea628095da3ee22c7125e8c095f2
hhrickyqi <455735031@qq.com>
2019-05-24 22:38:54 +0800

    practice

--- Test01/src/Test05.java
+++ Test01/src/Test05.java
@@ -1,10 +1,30 @@
 public class Test05 {
     public static void main(String[] args) {
+        try{
+            Drink drink = Drink.getDrink(4);
+            drink.taste();
+        } catch (DrinkNotFoundException e) {
+            System.out.println(new DrinkNotFoundException("null"));
+        }
 
     }
 }
 abstract class Drink{
     abstract void taste();
+    public static Drink getDrink(int drintType) throws DrinkNotFoundException{
+        if(drintType < 1 || drintType> 3){
+            throw new DrinkNotFoundException();
+        }
+        switch (drintType){
+            case 1:
+                return new Coffee();
+            case 2:
+                return new Bear();
+            case 3:
+                return new Milk();
+        }
+        return null;
+    }
 
 }
 class Coffee extends Drink{
