8ffbf464debbc66f978cc87f76d4ed826b5c9456
hhrickyqi <455735031@qq.com>
2020-08-15 00:02:56 +0800

    Test

--- Algorithm/src/TestDelete.java
+++ Algorithm/src/TestDelete.java
@@ -2,5 +2,7 @@ public class TestDelete {
     public static void main(String[] args) {
         System.out.println("delete");
         System.out.println("delete");
+        System.out.println("delete");
+        System.out.println("delete");
     }
 }
